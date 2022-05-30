d5bdf843d25f8611e8e75c2eccde3d21af6ebe7e
nakano ryo <>
2022-05-30 22:00:42 +0000

    first

---
+++ js/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+{
+    var pieData = {
+        labels: ['Webデザイナー','Webデベロッパー','サーバーエンジニア','営業職'],
+        series: [14, 9, 8, 6]
+    };
+    var pieOptions = {
+        width: '100%',
+        height: '440px'
+    };
+    new Chartist.Pie('.pie-chart', pieData, pieOptions);
+    
+    
+    
+    
+    var barData = {
+        labels: ['2018年','2019年','2020年','2021年'],
+        series: [[10,15,23,40]]
+    };
+    var barOptions = {
+        axisY:{
+          offset: 60,
+          scaleMinSpace: 50,
+          labelInterpolationFnc: function(value) {
+              return value + ' 人'
+            }
+          },
+        width:'100%',
+        height:'400px'
+    };    
+    new Chartist.Bar ('.bar-chart', barData, barOptions);
+    
+}
